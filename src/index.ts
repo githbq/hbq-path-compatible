90df33c5725565e62c74460ad544b08dfb4fb54d
hubiqing <hbqhbq123@qq.com>
2020-03-20 23:18:18 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import * as pathTool from 'path'
 
+const compatiblePathTool = {}
 
+Object.keys(pathTool).forEach(methodName => {
+  compatiblePathTool[methodName] = function(...args) {
+   let result = pathTool[methodName](...args)
+   if (typeof result === 'string') {
+    result = result.replace(/\\/g, '/')
+   }
+   return result
+  }
+})
 
-
-export default pathTool
+export default compatiblePathTool
