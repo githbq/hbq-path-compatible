e0de8307cfd132663a4e60d83345d7d29a7da259
hubiqing <hbqhbq123@qq.com>
2020-03-20 23:24:00 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import * as pathTool from 'path'
 const compatiblePathTool = {}
 
 Object.keys(pathTool).forEach(methodName => {
-  compatiblePathTool[methodName] = function(...args) {
-   let result = pathTool[methodName](...args)
-   if (typeof result === 'string') {
-    result = result.replace(/\\/g, '/')
-   }
-   return result
+  const property = pathTool[methodName]
+  if (typeof property !== 'function') {
+    compatiblePathTool[methodName] = property
+  } else {
+    compatiblePathTool[methodName] = function (...args) {
+      let result = property(...args)
+      if (typeof result === 'string') {
+        result = result.replace(/\\/g, '/')
+      }
+      return result
+    }
   }
 })
 
